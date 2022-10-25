7bae06b9dcb1a78cfa8d85076902947a310a24fc
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-25 09:15:50 +0200

    komentare

--- MojPrvy/src/MojPrvyProgram.java
+++ MojPrvy/src/MojPrvyProgram.java
@@ -3,7 +3,23 @@ public class MojPrvyProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ahoj 4B"); 
+		//jednoriadkovy komentar
+		System.out.println("Ahoj 4B");
+		
+		/*
+		 * viacriadkovy 
+		 * komentar
+		 */
+		
+		/**
+		 * 
+		 */
+		System.out.println(10 + 5);
+		System.out.println("10 + 5");
+		
+		System.out.println("A" + "B");
+		System.out.println("10" + 10);
+		System.out.println("10 + 5 = " + (10 + 5));
 		
 	}
 
